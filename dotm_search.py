60775930d0fff3e249bb0add3d70db93b495bc3b
Jordan Davidson <jordandavidson744@yahoo.com>
2020-02-25 11:22:42 -0500

    initial push

--- dotm_search.py
+++ dotm_search.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--directory' help = 'Specify a directory' required=True)
+parser.add_argument('-t', '--text', help = 'Specify search text' required=True)
+args = parser.parse_args()
 """
 Given a directory path, search all files in the path for a given text string
 within the 'word/document.xml' section of a MSWord .dotm file.
 """
-__author__ = "???"
+__author__ = "Jordan"
 
 
 def main():
@@ -14,3 +19,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
